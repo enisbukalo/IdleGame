b258ad4e147f694f299e261ed72d8153cb0a4ce8
Enis Bukalo <bukaloenis@gmail.com>
2022-11-13 13:59:01 -0500

    Cmake Fix

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "LibA/include/test.h"
+#include "LibB/include/testTwo.h"
 
 int main() {
+
+    TEST x = TEST();
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
